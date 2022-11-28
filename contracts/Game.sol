28be896111c984a6dd34f8d1ce1a2183015afb11
Mauricio Pisabarro <mauripisabarro@gmail.com>
2022-11-28 01:24:42 -0300

    Emit boss defeated event

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -25,6 +25,12 @@ contract Game is IGame {
     mapping(address => bool) private _userHasCharacter;
     mapping(int256 => bool) private _defeatedBosses;
 
+    event BossDefeated(
+        uint256 indexed characterId,
+        uint256 indexed bossId,
+        uint256 reward
+    );
+
     constructor() {
         _admin = msg.sender;
         _currentBoss = -1;
@@ -52,12 +58,18 @@ contract Game is IGame {
     }
 
     modifier onlyIfBossExists(uint256 _bossId) {
-        require(_bossId < _bosses.length && _bossId >= 0, "Boss does not exist");
+        require(
+            _bossId < _bosses.length && _bossId >= 0,
+            "Boss does not exist"
+        );
         _;
     }
 
     modifier onlyIfBossIsPopulated() {
-        require(_currentBoss >= 0 && uint256(_currentBoss) < _bosses.length, "Current boss not populated");
+        require(
+            _currentBoss >= 0 && uint256(_currentBoss) < _bosses.length,
+            "Current boss not populated"
+        );
         require(_defeatedBosses[_currentBoss], "Current boss already defeated");
         _;
     }
@@ -68,15 +80,9 @@ contract Game is IGame {
     }
 
     modifier onlyOwningExistingLivingCharacterWithXP() {
-        require(
-            _userHasCharacter[msg.sender],
-            "You need to own a character"
-        );
+        require(_userHasCharacter[msg.sender], "You need to own a character");
         Character character = _userCharacterMap[msg.sender];
-        require(
-            character.isDead() == false,
-            "You need a living character"
-        );
+        require(character.isDead() == false, "You need a living character");
         require(
             character.getExperiencePoints() > 0,
             "You need a character with XP"
@@ -96,7 +102,11 @@ contract Game is IGame {
         return _address == _admin;
     }
 
-    function createBoss(uint256 _hps, uint256 _reward, uint256 _damage) external onlyAdmin returns (uint256) {
+    function createBoss(
+        uint256 _hps,
+        uint256 _reward,
+        uint256 _damage
+    ) external onlyAdmin returns (uint256) {
         Boss boss = new Boss(this, _hps, msg.sender, _reward, _damage);
         _bosses.push(boss);
         uint256 playerId = _bosses.length - 1;
@@ -114,7 +124,11 @@ contract Game is IGame {
         _currentBoss = int256(_bossIndex);
     }
 
-    function createCharacter() external onlyOneCharacterPerAddress returns (uint256) {
+    function createCharacter()
+        external
+        onlyOneCharacterPerAddress
+        returns (uint256)
+    {
         Character character = new Character(this, defaultHp, msg.sender);
         _characters.push(character);
 
@@ -126,23 +140,35 @@ contract Game is IGame {
         return playerId;
     }
 
-    function attackBoss() external onlyIfBossIsPopulated needToOwnACharacter returns (bool) {
+    function attackBoss()
+        external
+        onlyIfBossIsPopulated
+        needToOwnACharacter
+        returns (bool)
+    {
         Character character = _userCharacterMap[msg.sender];
-        require(
-            !character.isDead(),
-            "Character is dead, can't attack"
-        );
+        require(!character.isDead(), "Character is dead, can't attack");
         Boss boss = _bosses[uint256(_currentBoss)];
 
         bool isBossDefeated = boss.receiveAttack(character.getAttackDamage());
         if (isBossDefeated) {
-            character.receiveExperiencePoints(boss.getDefeatedReward());
+            uint256 reward = boss.getDefeatedReward();
+            character.receiveExperiencePoints(reward);
             _defeatedBosses[_currentBoss] = true;
             _currentBoss = -1;
+
+            emit BossDefeated(
+                character.getId(),
+                boss.getId(),
+                boss.getDefeatedReward()
+            );
+
             return true;
         }
 
-        bool isCharacterDefeated = character.receiveAttack(boss.getAttackDamage());
+        bool isCharacterDefeated = character.receiveAttack(
+            boss.getAttackDamage()
+        );
         if (!isCharacterDefeated) {
             character.receiveExperiencePoints(boss.getOfferedReward());
         }
@@ -150,7 +176,11 @@ contract Game is IGame {
         return false;
     }
 
-    function heal(uint256 _characterId) onlyOwningExistingLivingCharacterWithXP characterExists(_characterId) external {
+    function heal(uint256 _characterId)
+        external
+        onlyOwningExistingLivingCharacterWithXP
+        characterExists(_characterId)
+    {
         Character owned = _userCharacterMap[msg.sender];
         Character toHeal = _characters[_characterId];
 
